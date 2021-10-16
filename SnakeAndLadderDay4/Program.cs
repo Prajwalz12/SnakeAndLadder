408e6e2d84d9e4de5c8244839466b33317a85ad4
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-17 00:08:20 +0530

    adding repeat till 100 program

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -1,33 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace UC3
+namespace UC4
 {
-    class Program
+    public class Dice
     {
-        static void Main(string[] args)
+        int user1 = 0;
+        int count = 0;
+        List<int> countStop1 = new List<int>();
+        public void FirstDice()
         {
-            var count = 0;
-            Console.WriteLine("User1 start at positon 0");
-            var random = new Random().Next(1, 7);
-            Console.WriteLine("Dice no is : " + random);
-
-            int Option = new Random().Next(3);
-            Console.WriteLine("Player checks for option : " + Option);
-
-            if (Option == 0)
+            Console.WriteLine("User1 starts from position 0");
+            while (this.user1 < 100)
             {
-                Console.WriteLine("Player checks for noplay - Position is : " + count);
-            }
-            else if (Option == 1)
-            {
-                Console.WriteLine("Player checks for ladder - Position is :" + count + random);
-            }
-            else
-            {
-                Console.WriteLine("Player checks for snake - Position is : " + (count - random));
-            }
+
+                int rand = new Random().Next(1, 7);
+                Console.WriteLine($"Dice Roll === {rand}");
+
+                this.countStop1.Add(rand);
 
 
+                int option = new Random().Next(3);
+
+                if (option == 0)
+                {
+                    Console.WriteLine("User1 checks for NoPlay");
+                    Console.WriteLine($"User1 1 Reach : {this.user1}");
+
+                }
+                if (option == 1)
+                {
+                    Console.WriteLine("User1 checks for Ladder");
+                    this.user1 = this.user1 + rand;
+                    Console.WriteLine($"User1 1 Reach : { this.user1}");
+                }
+                if (option == 2)
+                {
+                    Console.WriteLine("User1 checks for Snake");
+                    this.user1 = this.user1 - rand;
+                    Console.WriteLine($"User1 1 Reach : { this.user1}");
+                }
+
+                if (this.user1 < 0)
+                {
+                    user1 = 0;
+                    Console.WriteLine("User1 starts from position 0");
+                }
+
+                this.count++;
+
+
+            }
+        }
+        void show()
+        {
+            Console.WriteLine($"Dies Roll Out {this.countStop1.Count} Times");
+            Console.WriteLine($"Winning Position : {this.user1}");
+            foreach (int i in this.countStop1)
+            {
+                Console.Write($"{i},");
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Dice obj = new Dice();
+                obj.FirstDice();
+                obj.show();
+            }
         }
     }
 }
