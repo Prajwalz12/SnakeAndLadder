d20f6e816669876e10b7c6635ef9e572607f1257
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-17 00:06:10 +0530

    adding option check program

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 
-namespace UC2
+namespace UC3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var count = 0;
             Console.WriteLine("User1 start at positon 0");
-            Random random = new Random();
-            Console.WriteLine("Dice no is : " + random.Next(1, 7));
+            var random = new Random().Next(1, 7);
+            Console.WriteLine("Dice no is : " + random);
+
+            int Option = new Random().Next(3);
+            Console.WriteLine("Player checks for option : " + Option);
+
+            if (Option == 0)
+            {
+                Console.WriteLine("Player checks for noplay - Position is : " + count);
+            }
+            else if (Option == 1)
+            {
+                Console.WriteLine("Player checks for ladder - Position is :" + count + random);
+            }
+            else
+            {
+                Console.WriteLine("Player checks for snake - Position is : " + (count - random));
+            }
+
+
         }
     }
 }
